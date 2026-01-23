8e9283645d12362505f6f3e2d227a66f5262ec19
Farhan Izzuadi <rfir06@gmail.com>
2026-01-23 23:07:41 +0700

    done

--- components/database.js
+++ components/database.js
@@ -1,1 +1,70 @@
 console.log("hello world!");
+
+
+let database = [
+    {
+        id: 1,
+        nama: "Gecko",
+        stok: 5,
+        foto: "./images/gecko.jpg",
+        deskripsi: "-------"
+    },
+    {
+        id: 2,
+        nama: "Iguana",
+        stok: 2,
+        foto: "./images/iguana.jpg",
+        deskripsi: "-------"
+    },
+    {
+        id: 3,
+        nama: "Ular Python",
+        stok: 1,
+        foto: "./images/python.jpg",
+        deskripsi: "-------"
+    },
+    {
+        id: 4,
+        nama: "Dinosaurus",
+        stok: 5,
+        foto: "./images/gecko.jpg",
+        deskripsi: "-------"
+    },
+    {
+        id: 5,
+        nama: "Kura-kura",
+        stok: 2,
+        foto: "./images/iguana.jpg",
+        deskripsi: "-------"
+    },
+    {
+        id: 6,
+        nama: "Kadal Gurun",
+        stok: 1,
+        foto: "./images/python.jpg",
+        deskripsi: "-------"
+    },
+    {
+        id: 7,
+        nama: "Komodo",
+        stok: 5,
+        foto: "./images/gecko.jpg",
+        deskripsi: "-------"
+    },
+    {
+        id: 8,
+        nama: "Bunglon",
+        stok: 2,
+        foto: "./images/iguana.jpg",
+        deskripsi: "-------"
+    },
+    {
+        id: 9,
+        nama: "Tokek",
+        stok: 1,
+        foto: "./images/python.jpg",
+        deskripsi: "-------"
+    },
+];
+
+//deskripsi nya nanti statis aja
