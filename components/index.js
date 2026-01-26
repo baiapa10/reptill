87a3705e46c1e18045368c6d851870361cae5347
Farhan Izzuadi <rfir06@gmail.com>
2026-01-26 23:49:56 +0700

    done

--- components/index.js
+++ components/index.js
@@ -9,6 +9,25 @@ window.addEventListener("DOMContentLoaded", function () {
   readData();
 });
 
+// let banners = [
+//   "../images/banner1.jpg",
+//   "../images/banner2.jpg",
+//   "../images/banner3.jpg"
+// ];
+
+// let currentIndex = 0;
+// let bannerImg = document.getElementById("banner-img");
+
+// setInterval(function () {
+//   currentIndex++;
+
+//   if (currentIndex >= banners.length) {
+//     currentIndex = 0;
+//   }
+
+//   bannerImg.src = banners[currentIndex];
+// }, 6000);
+
 //Read data => DISIMPAN DALAM FUNCTION
 function readData() {
   //1. Buat template dengan string kosong
@@ -21,18 +40,34 @@ function readData() {
 
     //3. Didalam looping, concate variable template dengan elemen HTML
     template += `
-      <tr>
-        <td>${perObject.id}</td>
-        <td>${perObject.nama}</td>
-        <td>${perObject.stok}</td>
-        <td>${perObject.deskripsi}</td>
-        <td><img src="${perObject.foto}" alt="${perObject.nama}" style="width: 100px;"></td>
-      </tr>
+      <div class="col reptile-card">
+        <div class="card h-100">
+          <img 
+            src="${perObject.foto}" 
+            class="card-img-top card-img-custom" 
+            alt="${perObject.nama}"
+          >
+          <div class="card-body">
+            <h5 class="card-title">${perObject.nama}</h5>
+            <p class="card-text">
+              <strong>Stok:</strong> ${perObject.stok}<br>
+              <strong>Deskripsi:</strong> ${perObject.deskripsi}
+            </p>
+            <button 
+              class="btn btn-warning mt-auto"
+              onclick="editData(${perObject.id})"
+            >
+              Edit
+            </button>
+          </div>
+        </div>
+      </div>
     `;
   }
 
   //4. Diluar looping, get element yang akan menjadi container dari elemen template
-  let tbody = document.getElementById("container-data");
+  document.getElementById("container-data").innerHTML = template;
+  // let tbody = document.getElementById("container-data");
   tbody.innerHTML = template;
 }
 readData();
